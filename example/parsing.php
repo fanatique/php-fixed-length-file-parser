a35b6092b293b19636d6dd0d4fd053a42a2ea1dc
Alexander Thomas <alexander.thomas@verkstedt.com>
2026-03-19 09:21:16 +0100

    Updated annoyingly outdated md5 example

--- example/parsing.php
+++ example/parsing.php
@@ -1,14 +1,16 @@
 <?php
 
+/**
+ * This file is part of the php-fixed-length-file-parser package.
+ *
+ * @link    https://github.com/fanatique/php-fixed-length-file-parser
+ * @license MIT https://opensource.org/licenses/MIT
+ */
+
 declare(strict_types=1);
 
 /**
- * php-fixed-length-file-parser
- *
  * This script illustrates how to use the fixed length file parser.
- *
- * @link    https://github.com/fanatique/php-fixed-length-file-parser
- * @license MIT
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -28,10 +30,10 @@ $parser->setFilePath(__DIR__ . '/example.dat');
 
 // ## 1a. Optional features
 // Register a callable that determines if a line needs to be parsed.
-// This example ignores any line which md5 sum is f23f81318ef24f1ba4df4781d79b7849 (which kicks out Gilly)
-$linesToIgnore = ['f23f81318ef24f1ba4df4781d79b7849'];
+// This example ignores any line whose sha1 hash is 0d4705... (which kicks out Gilly)
+$linesToIgnore = ['0d470573afeae83690434c7eddd4f7b755ec5df9'];
 $parser->setPreflightCheck(function (string $currentLineStr) use ($linesToIgnore): bool {
-    return !in_array(md5($currentLineStr), $linesToIgnore, true);
+    return !in_array(sha1($currentLineStr), $linesToIgnore, true);
 });
 
 // Register a callback which is applied to each parsed line
